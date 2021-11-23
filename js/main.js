89a0ae4efe45ecde13619cd6cc61422384900827
Marcin Wiatrak <mwiatrak@gmail.com>
2021-11-23 17:57:07 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const hamburgerButton = document.querySelector('#hamburger-menu-button');
 const nav = document.querySelector('#nav');
 const body = document.querySelector('body');
 
+let lastScrollPosition = 0;
+
 const toggleMenu = () => {
     nav.classList.toggle('active');
     hamburgerButton.classList.toggle('active')
@@ -10,21 +12,20 @@ const toggleMenu = () => {
 const fn = () => {
     const scrollToTop = document.scrollingElement.scrollTop
     const xvalue = "center";
-    const factor = -0.04;
+    const factor = -0.02;
     const yvalue = scrollToTop * factor;
     body.style.backgroundPosition = xvalue + " " + yvalue + "px";
-    console.log(yvalue);
+
 }
 
+const showMenu = () => {
+    lastScrollPosition = window.scrollY;
+    console.log(lastScrollPosition);
+}
 
+window.addEventListener('scroll', showMenu)
 body.onscroll = () => fn();
 body.addEventListener('gesturechange', fn);
 body.addEventListener('scroll', fn);
 
 const userAgent = navigator.userAgent;
-let browserName;
-
-if(userAgent.match(/safari/i)){
-    console.log('safari');
-    console.log(userAgent);
-}
